764ee426228ccdcffad4265d78253f23f63dac57
haidarniv <haidarapps12@gmail.com>
2025-05-13 20:55:17 +0700

    progress a bit on index js first setup express js

--- TimeNestBackend/index.js
+++ TimeNestBackend/index.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req,res) => {
+    res.send('TimeNest API is Running');
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
