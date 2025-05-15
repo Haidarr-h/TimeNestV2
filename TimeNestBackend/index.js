d75a1e8453c1f92c834a2e370e5e52b50b04f77f
haidarniv <haidarapps12@gmail.com>
2025-05-15 18:46:42 +0700

    trying to connect mongodb, but still super confused

--- TimeNestBackend/index.js
+++ TimeNestBackend/index.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 const app = express();
+mongoose.connect('mongodb+srv://711haidarhanif:@cluster0.jyfyy1x.mongodb.net/testCrud')
+
+const userSchema = new mongoose.Schema({
+    name:String,
+    age:Number
+})
+
+
+const userModel = mongoose.model("emp", userSchema);
+
+const emp1 = new userModel({
+    name:"Rijushree",
+    age:23
+});
+
+emp1.save()
+  .then(() => {
+    console.log("Employee saved to DB ✅");
+  })
+  .catch((err) => {
+    console.error("Error saving employee ❌", err);
+  });
+
+
 app.use(cors());
 app.use(bodyParser.json());
 
